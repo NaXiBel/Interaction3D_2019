0a0c3c9feec6af28705b5e9cfa7acf8c2daef612
Julien <>
2019-01-21 22:40:30 +0100

    fixe menu dropdown

--- Unity Projet Reva/Assets/Resources/Scripts/DropDownController.cs
+++ Unity Projet Reva/Assets/Resources/Scripts/DropDownController.cs
@@ -5,11 +5,10 @@ using UnityEngine.UI;
 
 public class DropDownController : MonoBehaviour {
 
-	void Start()
+	void Awake()
     {
         
         this.GetComponent<Dropdown>().options.Clear();
-        DropDown dr = this.GetComponent<DropDown>();
         this.GetComponent<Dropdown>().options.Add(new Dropdown.OptionData() { text = "Oculus Touch" });
         this.GetComponent<Dropdown>().options.Add(new Dropdown.OptionData() { text = "Leap Motion " });
         
